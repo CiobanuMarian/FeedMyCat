4931b4f0ed9b6b05a75b649da53746780bd56d10
CiobanuMarian <c.marian98@yahoo.com>
2020-01-14 20:05:05 +0200

    Initial commit

---
+++ app/src/main/java/com/msolutions/MainActivity.java
@@ -0,0 +1,69 @@
+package com.msolutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnFeed;
+    public static String wifiModuleIp = "";
+    public static int wifiModulePort = 0;
+    public static String CMD = "0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        btnFeed = (Button) findViewById(R.id.btnFeed);
+        btnFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getIPandPort();
+                CMD = "Up";
+                Socket_AsyncTask cmd_increase_servo = new Socket_AsyncTask();
+                cmd_increase_servo.execute();
+            }
+        });
+        setContentView(R.layout.activity_main);
+
+    }
+    public void getIPandPort()
+    {
+        String iPandPort = "";
+        Log.d("MYTEST","IP String: "+ iPandPort);
+        String temp[]= iPandPort.split(":");
+        wifiModuleIp = temp[0];
+        wifiModulePort = Integer.valueOf(temp[1]);
+        Log.d("MY TEST","IP:" +wifiModuleIp);
+        Log.d("MY TEST","PORT:"+wifiModulePort);
+    }
+    public class Socket_AsyncTask extends AsyncTask<Void,Void,Void>
+    {
+        Socket socket;
+
+        @Override
+        protected Void doInBackground(Void... params){
+            try{
+                InetAddress inetAddress = InetAddress.getByName(MainActivity.wifiModuleIp);
+                socket = new java.net.Socket(inetAddress,MainActivity.wifiModulePort);
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeBytes(CMD);
+                dataOutputStream.close();
+                socket.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
